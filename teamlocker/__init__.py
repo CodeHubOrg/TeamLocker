2216166ae02fda94c6baf6f36ad44ceef12151bd
Stephen Leach <sfkleach@gmail.com>
2022-03-27 13:38:27 +0100

    Complete TG-8

--- teamlocker/__init__.py
+++ teamlocker/__init__.py
@@ -1,3 +1,4 @@
 __version__ = '0.1.0-dev'
 
 from teamlocker.client.client import run_client
+from teamlocker.server.server import run_server
---
+++ teamlocker/server/server.py
@@ -0,0 +1,9 @@
+# This is the code for the server program.
+
+from ..teamlocker import magic
+
+def run_server():
+    print( f"The magic is {magic(-100)}" )
+
+if __name__ == "__main__":
+    run_server()
